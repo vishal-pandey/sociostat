0ca1ae39df00f6541025635105ab95f296ca3bcf
VISHAL PANDEY <contact@vishalpandey.xyz>
2020-04-19 11:51:28 +0530

    pdates

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/',methods=['GET' , 'POST'])
 def index():
 	print("Hello world")
+	msg = "Hello World"
+	return render_template('index.html', message = msg)
 	
 if __name__ == "__main__":
   app.run(threaded=False)
